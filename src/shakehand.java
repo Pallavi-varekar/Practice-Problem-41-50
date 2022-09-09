a6daa4c816cd099793e87cf1e81ef295b85701ce
Pallavi-varekar <pallavivarekar4@gmail.com>
2022-09-09 22:31:18 +0530

    Initial commit

---
+++ src/shakehand.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class shakehand {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the number of person: ");
+        int N = sc.nextInt();
+        int handShakes=((N-1)*N)/2;
+        System.out.println("Maximum number of handshakes for "+N+" people is: "+handShakes);
+    }
+}
